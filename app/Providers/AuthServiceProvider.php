334cf02d1d82afba48389b17adb1f029ec5b3e5e
Emeshe Sotak <emeshe.sotak@pettersonapps.com>
2022-12-01 21:27:10 +0200

    lab4

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        'App\Models\Chat' => 'App\Policies\ChatPolicy',
+        'App\Models\ChatMember' => 'App\Policies\ChatMemberPolicy',
     ];
 
     /**
